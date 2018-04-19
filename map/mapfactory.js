75485e64f81568de361b4c37c98cfa23b2303097
Brad <brad.taris@gmail.com>
2018-04-19 14:20:08 -0700

    change, didn't show last time

--- map/mapfactory.js
+++ map/mapfactory.js
@@ -7,16 +7,16 @@
 function MapFactory()
 {
     this.maps = [];
-    //this.maps.push(SearchMap); // Simple random map
+    this.maps.push(SearchMap); // Simple random map
     this.maps.push(BspMap); // BSP rogue-like map
-    //this.maps.push(MazeMap);
+    this.maps.push(MazeMap);
 
     this.getMap = function(cols, rows, x, y, w, h, allowDiagonals, percentWalls)
     {
         if(this.maps.length == 0) return undefined;
 
-        var selected = floor(random(this.maps.length));
-        //var selected = floor(this.maps[1]));
+        //var selected = floor(random(this.maps.length));
+        var selected = floor(this.maps[1]);
         return new this.maps[selected](cols, rows, x, y, w, h, allowDiagonals, percentWalls);
     }
 }
